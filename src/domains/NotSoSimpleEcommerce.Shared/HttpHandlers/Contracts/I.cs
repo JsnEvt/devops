ea72c118e9371c770faef0c8e8a313342a9c3ecc
Jason Santos <110934550+JsnEvt@users.noreply.github.com>
2025-07-16 13:35:46 -0300

    Uso Refit - Header(token)

--- src/domains/NotSoSimpleEcommerce.Shared/HttpHandlers/Contracts/I.cs
+++ src/domains/NotSoSimpleEcommerce.Shared/HttpHandlers/Contracts/I.cs
@@ -4,8 +4,15 @@ using Refit;
 namespace NotSoSimpleEcommerce.Shared.HttpHandlers.Contracts;
 
 public interface IOrderApi
+//{
+//    [Get("/order/api/request/{id}")]
+//    [Headers("Authorization: Bearer")]
+//    Task<ApiResponse<OrderResponse>> GetOrderByIdAsync(int id);
+//}
+
 {
     [Get("/order/api/request/{id}")]
-    [Headers("Authorization: Bearer")]
-    Task<ApiResponse<OrderResponse>> GetOrderByIdAsync(int id);
+    [Headers("Authorization: Bearer (token)")]
+    Task<ApiResponse<OrderResponse>> GetOrderByIdAsync(int id, [Header("token"), string token]);
 }
+//codigo incompleto/correcao por usar o Refit
